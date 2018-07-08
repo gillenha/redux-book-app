90307d5c1f5c8a3eee18a79bf56c162eee1452fc
gillenha <gillen08@gmail.com>
2018-07-08 13:52:56 -0400

    first commits

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        <p>React Simple Starter</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
