7094d2d8368b278515973fac40949de112212a48
gillenha <gillen08@gmail.com>
2018-07-08 18:19:07 -0400

    even more styling

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,7 @@ import BookDetail from '../containers/book-detail';
 export default class App extends Component {
 	render() {
 		return (
-			<div className="flex flex-column">
+			<div className="flex flex-column ">
 			<h1 className="tc">My Favorite Books</h1>
 				<BookList />
 				<BookDetail className='tc' />
--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './book-list.css';
 
 class BookDetail extends Component {
 	render() {
